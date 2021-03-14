3c2c5a52bc06b857c15dca55e66f7288987d915a
lioboal <lioboal@gmail.com>
2021-03-14 22:27:13 +0900

    bootstrap 관련 오류 해결  - 의존성 추가  - 파일 내 한글 제거

--- src/main/java/com/madplay/springcloudconfigclient/service/DynamicConfigService.java
+++ src/main/java/com/madplay/springcloudconfigclient/service/DynamicConfigService.java
@@ -18,11 +18,14 @@ public class DynamicConfigService {
     private String profile;
     @Value("${taeng.comment}")
     private String comment;
+    @Value("${taeng.change}")
+    private String change;
 
     public Map<String, String> getConfig() {
         Map<String, String> map = new HashMap<>();
         map.put("profile", profile);
         map.put("comment", comment);
+        map.put("change", change);
         return map;
     }
 }
--- src/main/java/com/madplay/springcloudconfigclient/service/StaticConfigService.java
+++ src/main/java/com/madplay/springcloudconfigclient/service/StaticConfigService.java
@@ -16,11 +16,14 @@ public class StaticConfigService {
     private String profile;
     @Value("${taeng.comment}")
     private String comment;
+    @Value("${taeng.change}")
+    private String change;
 
     public Map<String, String> getConfig() {
         Map<String, String> map = new HashMap<>();
         map.put("profile", profile);
         map.put("comment", comment);
+        map.put("change", change);
         return map;
     }
 }
